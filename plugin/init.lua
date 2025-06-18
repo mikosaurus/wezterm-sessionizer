1dfa4090ea30bc253f7b95c34baaf28b63ea1c2c
Anders Johannessen <slatleim1991@gmail.com>
2025-06-18 19:44:43 +0000

    wip

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,7 +1,23 @@
-print("starting plugin....")
+local wezterm = require("wezterm")
+
+local function findPluginPackagePath(plugin)
+	local separator = package.config:sub(1, 1) == "\\" and "\\" or "/"
+	for _, v in ipairs(wezterm.plugin.list()) do
+		if v.url == plugin then
+			return v.plugin_dir .. separator .. "plugin" .. separator .. "?.lua"
+		end
+	end
+end
+
+package.path = package.path .. ";" .. findPluginPackagePath("https://github.com/mikosaurus/wezterm-sessionizer")
+
+local keys = require("keys")
 
 local M = {}
 
-M.apply_to_config = function(config) end
+M.apply_to_config = function(config)
+	config.leader = { key = "Space", mods = "CTRL" }
+	config.keys = keys.keys
+end
 
 return M
---
+++ plugin/keys.lua
@@ -0,0 +1,102 @@
+local wezterm = require("wezterm")
+local act = wezterm.action
+
+local M = {}
+M.keys = {
+
+	-- Section for tabs
+	{
+		key = "c",
+		mods = "LEADER",
+		action = act.SpawnTab("CurrentPaneDomain"),
+	},
+	{
+		key = "h",
+		mods = "ALT",
+		action = act.ActivateTabRelative(-1),
+	},
+	{
+		key = "l",
+		mods = "ALT",
+		action = act.ActivateTabRelative(1),
+	},
+
+	-- Splitting tab in panes
+	{
+		key = "æ",
+		mods = "ALT",
+		action = act.SplitPane({
+			direction = "Right",
+		}),
+	},
+	{
+		key = "'",
+		mods = "ALT",
+		action = act.SplitPane({
+			direction = "Down",
+		}),
+	},
+
+	-- Copy and paste
+	{
+		key = "C",
+		mods = "CTRL",
+		action = act.CopyTo("ClipboardAndPrimarySelection"),
+	},
+	{
+		key = "v",
+		mods = "CTRL",
+		action = act.PasteFrom("Clipboard"),
+	},
+
+	-- Activate copy mode
+	{
+		key = "ø",
+		mods = "LEADER",
+		action = act.ActivateCopyMode,
+	},
+
+	-- Section for moving between panes
+	{
+		key = "LeftArrow",
+		mods = "CTRL",
+		action = act.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "h",
+		mods = "CTRL",
+		action = act.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "RightArrow",
+		mods = "CTRL",
+		action = act.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "l",
+		mods = "CTRL",
+		action = act.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "UpArrow",
+		mods = "CTRL",
+		action = act.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "k",
+		mods = "CTRL",
+		action = act.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "DownArrow",
+		mods = "CTRL",
+		action = act.ActivatePaneDirection("Down"),
+	},
+	{
+		key = "j",
+		mods = "CTRL",
+		action = act.ActivatePaneDirection("Down"),
+	},
+}
+
+return M
