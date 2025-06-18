7d0f4d5d9b2e004ce24399e0d4b438fec97af0bf
Anders Johannessen <slatleim1991@gmail.com>
2025-06-18 20:34:53 +0000

    wip

--- plugin/init.lua
+++ plugin/init.lua
@@ -3,15 +3,14 @@ local wezterm = require("wezterm")
 local function findPluginPackagePath(plugin)
 	local separator = package.config:sub(1, 1) == "\\" and "\\" or "/"
 	for _, v in ipairs(wezterm.plugin.list()) do
+		print(v.url)
 		if v.url == plugin then
 			return v.plugin_dir .. separator .. "plugin" .. separator .. "?.lua"
 		end
 	end
 end
 
-package.path = package.path
-	.. ";"
-	.. findPluginPackagePath("httpssCssZssZsgithubsDscomsZsmikosaurussZswezterm-sessionizer")
+package.path = package.path .. ";" .. findPluginPackagePath("https://github.com/mikosaurus/wezterm-sessionizer")
 
 local keys = dofile("keys.lua")
 
