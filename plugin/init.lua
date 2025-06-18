120a97b681f636a9e59d533905171bb5a316f02e
Anders Johannessen <slatleim1991@gmail.com>
2025-06-18 19:44:43 +0000

    stuff going on

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,7 +1,10 @@
-print("starting plugin....")
+-- local wezterm = require("wezterm")
+local keys = require("keys")
 
 local M = {}
 
-M.apply_to_config = function(config) end
+M.apply_to_config = function(config)
+	config.key = keys.keys
+end
 
 return M
---
+++ plugin/keys.lua
@@ -0,0 +1,43 @@
+local M = {}
+M.keys = {
+	{
+		key = "LeftArrow",
+		mods = "CTRL",
+		actions = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "h",
+		mods = "CTRL",
+		actions = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "RightArrow",
+		mods = "CTRL",
+		actions = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "l",
+		mods = "CTRL",
+		actions = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "UpArrow",
+		mods = "CTRL",
+		actions = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "k",
+		mods = "CTRL",
+		actions = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "DownArrow",
+		mods = "CTRL",
+		actions = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{
+		key = "j",
+		mods = "CTRL",
+		actions = wezterm.action.ActivatePaneDirection("Down"),
+	},
+}
