f91d31ab26bca43114c500033b27f5d770ece043
Anders Johannessen <slatleim1991@gmail.com>
2025-06-18 20:18:07 +0000

    wip

--- plugin/init.lua
+++ plugin/init.lua
@@ -3,14 +3,10 @@ local wezterm = require("wezterm")
 local function findPluginPackagePath(plugin)
 	local separator = package.config:sub(1, 1) == "\\" and "\\" or "/"
 	for _, v in ipairs(wezterm.plugin.list()) do
-		return v.plugin_dir .. separator .. "plugin" .. separator .. "?.lua"
+		if v.url == plugin then
+			return v.plugin_dir .. separator .. "plugin" .. separator .. "?.lua"
+		end
 	end
-end
-
-local path = resolve_path()
-if not path then
-	wezterm.log_error("Plugin not found")
-	return
 end
 
 package.path = package.path
